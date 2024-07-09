1335a412304c5f4ebb709f8ba8f0e588fcf848c8
ctu-3u <lyii.ctu@gmail.com>
2024-07-09 22:38:19 +0800

    little practice for pygame image

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -11,6 +12,8 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
 
     while True:
 
@@ -21,6 +24,7 @@ def run_game():
         
         # re-draw screen in every cycle
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         pygame.display.flip()
 
