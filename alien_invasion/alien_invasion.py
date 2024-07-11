8d1d97b3c3c4fa01ebc114e91220b122267f5346
ctu-3u <lyii.ctu@gmail.com>
2024-07-11 22:40:34 +0800

    practice pygame: operate items

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,9 +1,11 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -13,20 +15,18 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
+    bullets = Group()
 
     while True:
 
         # monitor mouse and keyboard activities
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
+        gf.update_bullets(bullets)
         
         # re-draw screen in every cycle
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        pygame.display.flip()
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
     
 
